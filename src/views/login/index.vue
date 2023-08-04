cc413b2d32409fe4a898e941a86d6521bf37d5ee
Jared Sun <jaredsun@Jareds-MacBook-Pro.local>
2023-08-04 14:52:29 +0800

    登录页初始化

---
+++ src/views/login/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="login">
+        <div class="login-main">
+            <el-text size="large">用户登陆</el-text>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less"></style>
