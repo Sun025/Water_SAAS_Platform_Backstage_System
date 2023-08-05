e48e4e816435ec258c2797c474233f74f5e0d0fc
Jared <baibinsun@gmail.com>
2023-08-05 23:53:45 +0800

    SvgIcon component finsh

---
+++ src/components/svg-icon/index.vue
@@ -0,0 +1,52 @@
+<template>
+    <svg :style="svgStyle">
+        <use :xlink:href="iconNames" :fill="color"></use>
+    </svg>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface SvgInter {
+    width: string,
+    height: string
+}
+
+const props = defineProps({
+    iconName: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        default: ''
+    },
+    width: {
+        type: String,
+        default: '16px'
+    },
+    height: {
+        type: String,
+        default: '16px'
+    }
+});
+
+const iconNames = computed(():string => {
+    return `#icon-${props.iconName}`;
+});
+
+const svgStyle = computed((): SvgInter => {
+    return {
+        width: props.width,
+        height: props.height,
+    };
+});
+</script>
+
+<style scoped lang="less">
+svg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
--- src/views/manage/menu/menu_list/index.vue
+++ src/views/manage/menu/menu_list/index.vue
@@ -1,26 +1,40 @@
 <template>
     <div>
         <el-menu background-color="#1B1D29" text-color="#DCE8FF">
-            <el-menu-item>概览</el-menu-item>
+            <el-menu-item>
+                <SvgIcon iconName="ic_gaikuo"></SvgIcon>
+                <span>概览</span>
+            </el-menu-item>
             <el-sub-menu>
                 <template #title>
-                    <svg>
-                        <use xlink:href="#icon-ic_lease"></use>
-                    </svg>
+                    <SvgIcon iconName="ic_opera"></SvgIcon>
                     <span>运营管理</span>
                 </template>
                 <el-menu-item v-for="item of yunying_list">{{ item }}</el-menu-item>
             </el-sub-menu>
-            <el-menu-item>租赁管理</el-menu-item>
-            <el-menu-item>数据中心</el-menu-item>
-            <el-menu-item>产品配置</el-menu-item>
-            <el-menu-item>账号管理</el-menu-item>
+            <el-menu-item>
+                <SvgIcon iconName="ic_lease"></SvgIcon>
+                <span>租赁管理</span>
+            </el-menu-item>
+            <el-menu-item>
+                <SvgIcon iconName="ic_date"></SvgIcon>
+                <span>数据中心</span>
+            </el-menu-item>
+            <el-menu-item>
+                <SvgIcon iconName="ic_device"></SvgIcon>
+                <span>产品配置</span>
+            </el-menu-item>
+            <el-menu-item>
+                <SvgIcon iconName="ic_account"></SvgIcon>
+                <span>账号管理</span>
+            </el-menu-item>
         </el-menu>
     </div>
 </template>
 
 <script setup lang="ts">
 import { reactive } from 'vue';
+import SvgIcon from '@/components/svg-icon/index.vue'
 
 const yunying_list = reactive<Array<string>>([
     '用户管理',
@@ -32,4 +46,8 @@ const yunying_list = reactive<Array<string>>([
 ])
 </script>
 
-<style scoped></style>
+<style scoped>
+span {
+    margin-left: 20px;
+}
+</style>
