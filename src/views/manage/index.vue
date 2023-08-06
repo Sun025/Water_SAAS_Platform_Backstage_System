b36b39a38b0e6c860b66d96dc985c9da115883c0
Jared <baibinsun@gmail.com>
2023-08-06 23:56:08 +0800

    create PageHeader component

--- src/views/manage/index.vue
+++ src/views/manage/index.vue
@@ -1,22 +1,27 @@
 <template>
     <div class="manage_container">
         <Menu class="manage_menu"></Menu>
-        <div class="manage_main"></div>
+        <PageHeader class="manage_main"></PageHeader>
     </div>
 </template>
 
 <script setup lang="ts">
 import Menu from './menu/index.vue'
+import PageHeader from './page-header/index.vue'
 </script>
 
 <style scoped lang="less">
 .manage_container {
     width: 100%;
     min-height: 100vh;
+    display: flex;
     .manage_menu {
         width: 251px;
         height: 100vh;
         background-color: #1B1D29;
+    }
+    .manage_main {
+        flex: 1;
     }
 }
 </style>
